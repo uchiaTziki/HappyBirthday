08cd9131952d99b4d6144efcfcbeb3ee58e8f33e
Sion Sasson <sion.sasson@myheritage.com>
2020-02-11 13:15:51 +0200

    First commit with new project

---
+++ HappyBirthday/HappyBirthday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyBirthday
+//
+//  Created by Sion Sasson on 11/02/2020.
+//  Copyright © 2020 Sion Sasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
